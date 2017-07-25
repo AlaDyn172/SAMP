0badcb61a6247ff98801ae5823c32b3a9e8477cc
Andrei Elvis <name1_elvis@yahoo.com>
2017-07-25 17:51:03 +0300

    New version

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ sleep 3
 
 CHK_FILE="/home/container/samp03svr"
 if [ -f $CHK_FILE ]; then
-    echo "Executable of SAMP exists, not downloading. To update, delezte samp03svr."
+    echo "Executable of SAMP exists, not downloading. To update, delete samp03svr."
 else
     mkdir -p /home/container/.tmp-build
     cd /home/container/.tmp-build
@@ -14,20 +14,16 @@ else
     tar -zxf samp037svr_R2-1.tar.gz
 
     cp -r samp03/* /home/container/
-    echo "Copiado samp03 SI"
     rm -r /home/container/.tmp-build
-    echo "Removido .tmpbuild SI"
     cd /home/container
-    echo "cd home container SI"
     chmod 700 *
-    echo "CHMOD 700 TODO SI"
 fi
 
 if [ -f "/home/container/server.cfg" ]; then
-    echo "server.cfg exists, not generating file."
+    echo "Server.cfg file exists, not creating another."
 else
     echo "lanmode 0
-rcon_password changeme
+rcon_password ChangePasword!@#!@#
 maxplayers 50
 port ${SERVER_PORT}
 hostname SA-MP 0.3 Server
@@ -44,8 +40,6 @@ stream_distance 300.0
 stream_rate 1000" > server.cfg
 fi
 
-    echo "DE NUEVO IR A HOME CONTAINER"
 cd /home/container
-MODIFIED_STARTUP=`echo ${STARTUP} | perl -pe 's@\{\{(.*?)\}\}@$ENV{$1}@g'`
-        echo "EMEPZANDO SERVIDOR"
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 ./samp03svr
